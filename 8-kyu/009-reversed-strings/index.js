f7df8c4094399698bb31700bcf21179b144cec19
andrewlosseff <andrewlosseff@gmail.com>
2020-09-01 09:49:15 +0300

    start on solving with JS and TS

---
+++ 8-kyu/009-reversed-strings/index.js
@@ -0,0 +1,15 @@
+// // Loop
+function solution(str) {
+    
+}
+
+// // Reverse join
+// function solution(str){
+//   return str.split('').reverse().join('');  
+// }
+
+// function solution(str){
+//   return [...str].reverse().join("")
+// }
+
+console.log(solution("world"))
