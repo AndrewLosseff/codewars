ff48b43cca547f33549898602a25c1479938924c
andrewlosseff <andrewlosseff@gmail.com>
2020-09-02 10:26:51 +0300

    add Python solution

---
+++ 8-kyu/006-find-the-smallest-integer-in-the-array/index.py
@@ -0,0 +1,34 @@
+# Loop
+def find_smallest_int(arr):
+    smallest = arr[0]
+    for num in arr:
+        if(num < smallest):
+            smallest = num
+    return smallest
+
+# Sort
+
+
+def find_smallest_int(arr):
+    arr.sort()
+    return arr[0]
+
+
+def find_smallest_int(arr):
+    return sorted(arr)[0]
+
+
+Reducer
+
+
+def find_smallest_int(arr):
+    return reduce(lambda acc, cur: acc if acc < cur else cur, arr)
+
+# Min
+
+
+def find_smallest_int(arr):
+    return min(arr)
+
+
+print(find_smallest_int([78, 56, -2, 12, 8, -33]))
