c5f89f858a102b6b378978c6e2d3fcb4a07625ce
andrewlosseff <andrewlosseff@gmail.com>
2020-09-08 10:24:59 +0300

    add 009-reversed-strings TS solution

--- 8-kyu/009-reversed-strings/index.ts
+++ 8-kyu/009-reversed-strings/index.ts
@@ -1,8 +1,41 @@
-// // Split and Join
-// export function solution(str: string): string {
-//     return str.split("").reverse().join("");
-// }
+// Loop
+export function solution(str: string): string {
+    let answer: string = ""
+    for(let i = str.length - 1; i >= 0; i--) {
+        answer += str[i]
+    }
+    return answer; 
+}
+
+export function solution(str: string): string {
+    let answer: string[] = []
+    for(let i = 0; i < str.length; i++) {
+        const char = str[i]
+        answer.unshift(char)
+    }
+    console.log(answer)
+    return answer.join('')
+}
+
+// Reverse join
+export function solution(str: string): string {
+    return str.split("").reverse().join("")
+}
+
+// Recurtion
+export function solution(str: string): string {
+    return str.length > 0 ? solution(str.substring(1)) + str.charAt(0) : '';
+}
+
+// Reduce
+export function solution(str: string): string {
+    return str.split("").reduceRight((acc, cur) => acc + cur)
+}
+
+// Map
+export function solution(str: string): string {
+    return str.split("").map((char, index, arr) => arr[arr.length - index - 1]).join("")
+}
 
 
-
-// console.log(solution("world"))
+console.log(solution("world"))
