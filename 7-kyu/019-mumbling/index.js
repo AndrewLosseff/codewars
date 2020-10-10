561e8dce822acaddeb7d69303e4fdb8e9f3ebccf
andrewlosseff <andrewlosseff@gmail.com>
2020-10-10 16:05:30 +0300

    add js solution

---
+++ 7-kyu/019-mumbling/index.js
@@ -0,0 +1,34 @@
+// loop
+function accum(s) {
+  const result = []
+  const lowerStr = s.toLowerCase()
+
+  for(let i = 0; i < lowerStr.length; i++) {
+    let str = lowerStr[i].toUpperCase()
+    for(let j = 0; j < i; j++) {
+      str += lowerStr[i]
+    }
+    result.push(str)
+  }
+
+  return result.join("-")
+}
+
+// map
+function accum(s) {
+  return [...s].map((char, index) => (char.toUpperCase() + char.toLowerCase().repeat(index))).join("-")
+}
+
+// reduce
+function accum(s) {
+  return [...s.toUpperCase()].reduce((acc, cur, index) => acc + "-" + cur + cur.toLowerCase().repeat(index))
+}
+function accum(s) {
+  return [...s.toLowerCase()].reduce((acc, cur, index) => acc + (index != 0 ? "-" : "") + cur.toUpperCase() + cur.repeat(index), "")
+}
+
+console.log(accum("ZpglnRxqenU")) // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("NyffsGeyylB")) // "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum("MjtkuBovqrU")) // "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+console.log(accum("EvidjUnokmM")) // "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+console.log(accum("HbideVbxncC")) // "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
