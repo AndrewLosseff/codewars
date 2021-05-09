dd570cb13fa3139f54bd59aae562a004c6e3a59a
andrewlosseff <andrewlosseff@gmail.com>
2021-05-09 09:28:43 +0300

    update solutions for multiples-of-3-or-5

--- 6-kyu/multiples-of-3-or-5/index.js
+++ 6-kyu/multiples-of-3-or-5/index.js
@@ -1,28 +1,72 @@
-//loop
-function solution(number){
-    let counter = 0
-    for(let i = 3; i < number; i++) {
-        if(i % 3 === 0 || i % 5 === 0) {
-            counter += i
-        }
+// for loop # 1
+function solution(number) {
+  
+  const tempArr = []
+  let sum = 0
+
+  for(let i = 0; i < number; i++) {
+    tempArr.push(i)
+  }
+
+  for(let i = 0; i < tempArr.length; i++) {
+    if(tempArr[i] % 3 === 0 || tempArr[i] % 5 === 0) {
+        sum += tempArr[i]
     }
-    return counter
+  }
+
+  return sum
+
 }
 
-// map + reduce
+// for loop # 2
+function solution(number){
+    let sum = 0
+    for(let i = 3; i < number; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            sum += i
+        }
+    }
+    return sum
+}
+
+// map
 function solution(number) {
-    return number < 3 ? 0 : Array.from({length: number}, (_,i) => i++).map(i => i % 3 === 0 || i % 5 === 0 ? i : 0 ).reduce((acc, cur) => acc + cur, 0)
+
+    const newArr = [...Array(number).keys()]
+    let sum = 0
+
+    function result(num) {
+        if(num % 3 === 0 || num % 5 === 0) {
+            sum += num
+        }
+    }
+    
+    newArr.map(num => result(num))
+    
+    return sum
 }
 
 // filter + reduce
 function solution(number) {
-    return number < 3 ? 0 : [...Array(number).keys()].filter(i => i % 3 === 0 || i % 5 === 0).reduce((acc, cur) => acc + cur, 0)
-   
+
+    const newArr = [...Array(number).keys()]
+    
+    const filteredArr = newArr.filter(num => num % 3 === 0 || num % 5 === 0)
+    
+    const sum = filteredArr.reduce((acc, cur) => acc + cur, 0)
+    
+    return sum
 }
+
 
 // reduce
 function solution(number) {
-    return number < 3 ? 0 : [...Array(number)].reduce((acc, _, idx) => acc + idx * (!(idx % 3) || !(idx % 5)), 0)
+
+    const newArr = [...Array(number).keys()]
+    
+    const sum = newArr.reduce((acc, _, idx) => acc + (!(idx % 3) || !(idx % 5) ? idx : 0), 0)
+    
+    return sum
 }
 
 console.log(solution(10)) // 23
--- playground/index.js
+++
@@ -1,8 +0,0 @@
-function positiveSum(arr) {
-    
-}
-
-
-// Cases
-console.log(positiveSum([1,-2,3,4,5]));
-console.log(positiveSum([-1,-2,-3,-4,-5]));
